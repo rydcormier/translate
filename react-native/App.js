1629fd1c2be7033c8d74960828148096b5f2fb19
Ryan Cormier <rydcormier@gmail.com>
2020-09-10 13:28:46 -0400

    fix header in App.js

--- react-native/App.js
+++ react-native/App.js
@@ -19,7 +19,7 @@ import {
 import {
     Header,
     Colors,
-    LanguagePicker 
+    LanguagePicker
 } from './src/components';
 
 const App: () => React$Node = () => {
@@ -31,11 +31,6 @@ const App: () => React$Node = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
 
           {/* body */}
           <View style={styles.body}>

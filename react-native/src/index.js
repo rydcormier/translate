4512d048d7149f456773eefc21d9fcc12b594752
Ryan Cormier <rydcormier@gmail.com>
2020-09-10 12:11:40 -0400

    Add language support and a picker.

---
+++ react-native/src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Ryan Cormier 2020
+ *
+ * This source code is licensed under the MIT license.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import Header from './components/Header';
+import Colors from './components/Colors';
+import LanguagePicker from './components/LanguagePicker';
+
+export {Header, Colors, LanguagePicker};

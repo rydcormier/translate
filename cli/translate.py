ecc89a8a8860ef48c85091ce4b2c3da2383d78b2
Ryan Cormier <rydcormier@gmail.com>
2020-09-15 13:17:36 -0400

    Clean up code and comments in translate.py

--- cli/translate.py
+++ cli/translate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
 # translate.py
 #
@@ -6,132 +6,140 @@
 # using the Google Translation API. Given a source text, return the translated
 # text.
 #
+#   Ryan Cormier <rydcormier@gmail.com>
+#   8/15/20
 
-from getopt import getopt, GetoptError
+import getopt
 import json.load
-import urllib
 import os
 import sys
+import urllib
 
 
-help_msg = """usage:  translate.py [options] --targ <LANG> [TEXT]
+help_msg = """usage:  translate.py [options] (-t | --target) <LANG> [TEXT]
 
 Options:
 
     --help, -h          Print this help message.
-    --infile, -i FILE   Read source text from FILE.
+    --input, -i FILE    Read source text from FILE.
     --list, -l          List supported languages and corresponding codes.
-    --outfile, -o FILE  Write target text to FILE.
-    --src, -s LANG      Language or language code of source text. If not given,
+    --output, -o FILE   Write target text to FILE.
+    --source, -s LANG   Language or language code of source text. If not given,
 						the translator will try to determine the source language.
-    --targ, -t LANG     Language or Language code of the target text.
+    --target, -t LANG   Language or Language code of the target text.
 """
 
 # Provided API
-URL = """https://translate.google.com/translate_a/single?client=at&dt=t&dt=\
+API = {
+'url': """https://translate.google.com/translate_a/single?client=at&dt=t&dt=\
 ld&dt=qca&dt=rm&dt=bd&dj=1&hl=%25s&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b\
-944-fa62-4b55-b330-74909a99969e&"""
-
-HEADER = {
+944-fa62-4b55-b330-74909a99969e&""",
+'header': {
     'Content-Type': 'application/x-www-form-urlencoded',
     'User-Agent':
-    'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1'
+    'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1'},
+'method': 'POST'
 }
 
-METHOD = 'POST'
 
-
-
-
-def get_opts():
-	"""Process and return command line options as a dict object."""
-	# get language data
-	data = get_lang_data()
-
-	try:
-		opts, args = getopt.getopt(
-			sys.argv[ 1 : ],
-			'hi:lo:s:t:',
-			[ 'help', 'infile=', 'list', 'outfile=', 'src=', 'targ=' ])
-	except  GetoptError as e:
-		return { 'error ': e}
-	for o, a in opts:
-		try:
-			if o in ("-h", "--help"):
-				return { 'help' : True }
-			elif o in ("-l", "--list"):
-				return { 'list': LanguageSupport.languages()}
-			elif o in ("-i", "--infile"):
-				pass # check if readabel
-			elif o in ("-o", "--outfile"):
-				pass # check permissions
-			elif o in ("-t", "--targ"):
-				pass
-			elif o in ("-s", "--src"):
-				pass
-		except Exception as e:
-			return { 'error': e }
-
-	# Verify that the opts are valid
-	if 'targ' not in opts:
-		return {'error': LanguageError('No target language given')}
-	if 'src' not in opts:
-		opts['src'] = 'auto'
-
-	return opts
-
-def get_request(source, target, text):
-    # define and encode request data
-    data = { 'sl': source, 'tl': target, 'q': text }
-    data = urllib.parse.urlencode(data).encode('ascii')
-
-    # return Request object
-    return urllib.request.Request(URL, header=HEADER, data=data, method=METHOD)
-
-
-# Process command line arguments and request translation from Google. Exits
-# with 0 on success and 1 otherwise.
-def main() :
-    ## get options
-	opts = get_opts()
-
-	if 'error' in opts:
-		sys.stderr.write(str(e) + '\n' + help_msg +'\n')
-		sys.exit(1)
-
-	if 'list' in opts:
-		for l, c in opts['list']:
-			print('%-21s  %s\n' % (l, c))
-			sys.exit(0)
-
-	if 'help' in opts:
-		print(help_msg)
-		sys.exit(0)
-
-    obj = {}
-	target = ''
-    kwargs = {'source': opts['src'], 'target': opts['targ'], 'text', opts['text'] }
+def getOpts():
+    """Process command line arguments and return a dict mapping of options."""
+    # get arguments
     try:
-        request = get_request(**kwaargs)
-        with urllib.request.urlopen(request) as resp:
-            data = json.load(resp)
+        opts, args = getopt.getopt(sys.argv[1:], 'hi:lo:s:t:',
+            ['help', 'input=', 'list', 'output=', 'source=', 'target='])
+    except GetoptError as e:
+        return { 'error': e.msg }
 
-        target = ' '.join(s['trans'] for s in data[sentences])
-
-        if 'ofile' in opts:
-            with open(opts['ofile'], 'w') as f:
-                f.write(target)
-        else:
-            print(target)
-    except PermissionError:
-        sys.stderr.write('Unable to write to ' + opts['ofile'] + '\n')
-        sys.exit(1)
+    #
+    # get supported languages: for now, load json file
+    try:
+        with open('languages.json', 'r') as f:
+            data = json.load(f)
     except:
-        sys.stderr.write('HTTP error\n')
+        return { 'error' : 'Unable to load language data.' }
+
+    # parse opts and verify
+    res = {}
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            return { 'help' : True }
+        elif o in ("-l", "--list"):
+            return { "list" : data }
+        elif o in ("-i", "--input"):
+            try:
+                with open(a, 'r') as f:
+                    res['text'] = f.read()
+            except:
+                return { 'error' : 'Unable to read from {}'.format(a) }
+        elif o in ("-o", "--output"):
+            # leave verification for later
+            res['output'] = a
+        elif o in ("-s", "--source"):
+            # make sure the language is supported
+            if a in data['languages'] or a in data['langcodes']:
+                res['source'] = a
+            else:
+                return { 'error': 'Unsupported language: {}'.format(a) }
+        elif o in ("-t", "--target"):
+            # verify language
+            if a in data['languages'] or a in data['langcodes']:
+                res['target'] = a
+            else:
+                return { 'error': 'Unsupported language: {}'.format(a) }
+
+    # target is a required
+    if 'target' not in res:
+        return { 'error' : 'Required argument missing.' }
+
+    # source is optional
+    if 'source' not in res:
+        res['source'] = 'auto'
+
+    # if no input file given, text is the last argument
+    if 'input' not in res:
+        if not args:
+            return { 'error' : 'No text given.'}
+        res['text'] = args
+
+    return res
+
+
+def main():
+    """Get options, make an HTTP request, parse the results and return the
+    translation"""
+    opts = getOpts()
+
+    if 'error' in opts:
+        sys.stderror.write('Error: {}\n{}\n'.format(res['error'], help_msg))
         sys.exit(1)
+
+    # encode data and use API to make HTTP request
+    data = { 'sl': opts['source'], 'tl': opts['target'], 'q': opts['text'] }
+    API['data'] = urllib.parse.urlencode(data).encode('ascii')
+    request = urllib.request.Request(**API)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            translation = json.load(respnse)
+    except:
+        sys.stderror.write('Unable to process HTTP request\n')
+        sys.exit(1)
+
+    # we want the 'trans' items in 'sentences'
+    target = ' '.join(s['trans'] for s in translation['sentences'])
+
+    if 'output' in opts:
+        try:
+            with open(opts['output'], 'w') as f:
+                f.write(target)
+        except:
+            sys.stderr.write('Unable to write to {}\n'.format(opts['out']))
+            sys.exit(1)
+    else:
+        print(target)
     sys.exit(0)
 
 
-
-if __name__ == '__main__' :
+if __name__ == "__main__":
     main()

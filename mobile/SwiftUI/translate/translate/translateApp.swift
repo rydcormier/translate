f990dce12437e6ed4443eb032d8b54dd573613e4
Ryan Cormier <rydcormier@gmail.com>
2020-09-21 10:49:24 -0400

    housekeeping

---
+++ mobile/SwiftUI/translate/translate/translateApp.swift
@@ -0,0 +1,17 @@
+//
+//  translateApp.swift
+//  translate
+//
+//  Created by Ryan Cormier on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct translateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

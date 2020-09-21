f20e2dc3023dc6a829d6fb69a10e6d1e16b6673a
Ryan Cormier <rydcormier@gmail.com>
2020-09-21 10:44:42 -0400

    Add mobile directory.

---
+++ mobile/react-native/App.js
@@ -0,0 +1,56 @@
+/**
+* Transate: React Native app
+*
+* @format
+* @flow strict-local
+*/
+
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Button
+} from 'react-native';
+
+import { Picker } from '@react-native-community/picker';
+
+import {
+    Header,
+    Colors,
+    Controller,
+    styles
+} from './src/Components';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={styles.scrollView}>
+                <Header />
+
+                {/* body */}
+                <View style={styles.body}>
+
+                    {/* controller */}
+                    <View style={styles.container}>
+                        <Controller />
+                    </View>
+                    {/* END controller */}
+
+                </View>
+                {/* END body*/}
+
+            </ScrollView>
+        </SafeAreaView>
+        </>
+);
+};
+
+export default App;
---
+++ mobile/react-native/src/Components/index.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Ryan Cormier 2020
+ *
+ * This source code is licensed under the MIT license.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import Header from './Header';
+import Colors from './Colors';
+import Controller from './Controller';
+import styles from './styles';
+
+export {
+    Header,
+    Colors,
+    Controller,
+    styles,
+};
